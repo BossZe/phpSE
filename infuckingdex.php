f938d9bcbef14565f718d90ea1fa84692fde5229
boss23150 <boss124725@gmail.com>
2021-12-24 09:57:53 +0700

    :trollface:

--- infuckingdex.php
+++ infuckingdex.php
@@ -11,7 +11,7 @@
         <head>
         <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #357;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">gaming</a>
+    <a class="navbar-brand" href="#">Gaming</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
